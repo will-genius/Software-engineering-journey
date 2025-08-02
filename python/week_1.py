b6e198adf6e3609ed6c07c31d79e932113eed3ba
will-genius <kawirawilkister@gmail.com>
2025-08-02 19:22:28 +0300

    python basic calculator

---
+++ python/week_1.py
@@ -0,0 +1,27 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+
+else:
+    print("Invalid operation. Please choose +, -, *, or /.")
